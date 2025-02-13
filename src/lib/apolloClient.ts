c1d3952a24bdf4de32e3aab6a27c886fe684da90
Ping-Hsuan-Yu <yu.pinghsuan@gmail.com>
2025-02-13 17:10:17 +0800

    fix: correct API URL construction in Apollo Client configuration

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/graphql",
+    uri: `${process.env.NEXT_PUBLIC_API_URL}/api/graphql` || "http://localhost:3000/api/graphql",
     credentials: "same-origin",
   }),
   cache: new InMemoryCache(),
